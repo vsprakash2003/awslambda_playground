ac4d44e131b611b599027ea37e0ee78d01d97ba0
Mycents <prakashsubramani@hotmail.com>
2019-11-19 15:29:45 -0500

    Added dummy URL for a company that delivers pizza and updated create-order

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,24 +1,42 @@
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
-const uuid = require("uuid");
+const rp = require("minimal-request-promise");
 
 /* check if the order has relevant details supplied, if not throw error */
 function createOrder(request) {
   if (!request || !request.pizzaId || !request.address)
     throw new Error(
-      "You must provide either an order id or an address associated with the order"
+      "To order pizza please provide pizza type and address where pizza should be delivered"
     );
-  return docClient
-    .put({
-      TableName: "pizza-orders",
-      Item: {
-        orderId: uuid(),
-        pizza: request.pizza,
-        address: request.address,
-        orderStatus: "pending"
-      }
+
+  return rp
+    .post("https://some-like-it-hot.effortless-serverless.com/delivery", {
+      headers: {
+        Authorization: "aunt-marias-pizzeria-1234567890",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        pickupTime: "15.34pm",
+        pickupAddress: "Aunt Maria Pizzeria",
+        deliveryAddress: request.address,
+        webhookUrl:
+          "https://vi8yeybnz0.execute-api.us-east-2.amazonaws.com/latest/delivery"
+      })
     })
-    .promise()
+    .then(rawResponse => JSON.parse(rawResponse.body))
+    .then(response =>
+      docClient
+        .put({
+          TableName: "pizza-orders",
+          Item: {
+            orderId: response.deliveryId,
+            pizza: request.pizza,
+            address: request.address,
+            orderStatus: "pending"
+          }
+        })
+        .promise()
+    )
     .then(res => {
       console.log("Order is saved!", res);
       return res;
