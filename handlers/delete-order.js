c5f2db83c2de926dc65c3f968b4334a16f377743
Mycents <prakashsubramani@hotmail.com>
2019-11-21 10:21:41 -0500

    updated deleteorder handler with code for deleting a delivery if the order is pending

--- handlers/delete-order.js
+++ handlers/delete-order.js
@@ -1,24 +1,41 @@
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
+const rp = require("minimal-request-promise");
 
 /* delete the order based on order id */
 function deleteOrder(orderId) {
   return docClient
-    .delete({
+    .get({
       TableName: "pizza-orders",
       Key: {
         orderId
       }
     })
     .promise()
-    .then(res => {
-      console.log("Order is deleted!", res);
-      return res;
+    .then(result => result.item)
+    .then(item => {
+      if (item.orderStatus !== "pending")
+        throw new Error("Order status is not pending");
+
+      return rp.delete(
+        `https://some-like-it-hot.effortless-serverless.com/delivery/${orderId}`,
+        {
+          headers: {
+            Authorization: "aunt-marias-pizzeria-1234567890",
+            "Content-Type": "application/json"
+          }
+        }
+      );
     })
-    .catch(deleteError => {
-      console.log(`Oops, order is not deleted :(`, deleteError);
-      throw deleteError;
-    });
+    .then(() =>
+      docClient.delete({
+        TableName: "pizza-orders",
+        Key: {
+          orderId
+        }
+      })
+    )
+    .promise();
 }
 
 module.exports = deleteOrder;
